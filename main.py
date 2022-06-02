c0ee70cb98f445d6c1ab802cf8e7c69df07a8d34
BuildTools <unconfigured@null.spigotmc.org>
2022-06-02 12:58:19 -0700

    Leaderboard and utils added to flask

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ def index():
     with open('index.html', 'r') as f:
         return f.read()
 
+@app.route('/util.js')
+def utils():
+    with open('util.js', 'r') as f:
+        return f.read()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
