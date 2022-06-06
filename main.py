d1c47ae89a507bc37d05674c073536e39f51d36b
BuildTools <unconfigured@null.spigotmc.org>
2022-06-06 07:36:34 -0700

    temporarily removed scores and leaderboard

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import json
 from flask import Flask
 app = Flask(__name__)
-scores = None
-with open('scores.json', 'r') as f:
-    scores = json.load(f)
 @app.route('/')
 def index():
     with open('index.html', 'r') as f:
@@ -14,21 +11,5 @@ def utils():
     with open('util.js', 'r') as f:
         return f.read()
 
-@app.route('/leaderboard')
-def leaderboard():
-    with open('leaderboard.html', 'r') as f:
-        return f.read()
-
-@app.route('/success/<name>')
-def success(name):
-    scores[name] += 1
-    print(scores)
-    # Save scores to file
-    return str(scores[name])
-
 if __name__ == '__main__':
-    try:
-        app.run(host='0.0.0.0', port=80, debug=True)
-    except:
-        with open('scores.json', 'w') as f:
-            json.dump(scores, f)
+    app.run(host='0.0.0.0', port=80, debug=True)
